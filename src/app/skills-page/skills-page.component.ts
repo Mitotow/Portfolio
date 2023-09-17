99e841503c03b7875eec6e67da67301c7ad07837
Thomas <thomas@Thomas-HP-Laptop.home>
2023-09-17 10:47:10 +0200

    skills - first version
    - Change position of nav from absolute to fixed\n- Add keyframe growIn

--- src/app/skills-page/skills-page.component.ts
+++ src/app/skills-page/skills-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Usage from 'src/interfaces/Usage';
 
 @Component({
   selector: 'app-skills-page',
@@ -6,5 +7,84 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills-page.component.scss']
 })
 export class SkillsPageComponent {
+  languages:Usage[]=[
+    {
+      iconClass: 'devicon-html5-plain colored',
+      name: 'HTML5',
+    },
+    {
+      iconClass: 'devicon-css3-plain colored',
+      name: 'CSS3',
+    },
+    {
+      iconClass: 'devicon-javascript-plain colored',
+      name: 'Javascript',
+    },
+    {
+      iconClass: 'devicon-typescript-plain colored',
+      name: 'Typescript',
+    },
+    {
+      iconClass: 'devicon-php-plain colored',
+      name: 'PHP',
+    },
+    {
+      iconClass: 'devicon-java-plain colored',
+      name: 'Java',
+    },
+    {
+      iconClass: 'devicon-kotlin-plain colored',
+      name: 'Kotlin',
+    },
+    {
+      iconClass: 'devicon-csharp-plain colored',
+      name: 'Csharp',
+    },
+    {
+      iconClass: 'devicon-go-original-wordmark colored',
+      name: 'Golang',
+    },
+  ]
 
+  tools:Usage[]=[
+    {
+      iconClass: 'devicon-git-plain colored',
+      name: 'git',
+    },
+    {
+      iconClass: 'devicon-postgresql-plain colored',
+      name: 'Postgresql',
+    },
+    {
+      iconClass: 'devicon-jetbrains-plain colored',
+      name: 'Jerbrains',
+    },
+    {
+      iconClass: 'devicon-visualstudio-plain colored',
+      name: 'Visual Studio',
+    },
+    {
+      iconClass: 'devicon-vscode-plain colored',
+      name: 'Visual Studio Code',
+    },
+  ]
+
+  frameworks:Usage[]=[
+    {
+      iconClass: 'devicon-angularjs-plain colored',
+      name: 'Angular',
+    },
+    {
+      iconClass: 'devicon-react-plain colored',
+      name: 'React',
+    },
+    {
+      iconClass: 'devicon-nodejs-plain colored',
+      name: 'NodeJS',
+    },
+    {
+      iconClass: 'devicon-dotnetcore-plain colored',
+      name: '.NET',
+    },
+  ]
 }
---
+++ src/interfaces/Usage.ts
@@ -0,0 +1,4 @@
+export default interface Usage {
+    iconClass:string,
+    name:string,
+}
