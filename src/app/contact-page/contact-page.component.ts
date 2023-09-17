eb9fdae61f98c98554bcc75bd569ad1e99db2b55
Thomas <thomas@Thomas-HP-Laptop.home>
2023-09-17 17:23:25 +0200

    contact - first version

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-contact-page',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-page.component.scss']
 })
 export class ContactPageComponent {
+  constructor(private clip : Clipboard) {}
 
+  alert = "copyVerif";
+  alertText = "Cliquez pour copier";
+
+  onMouseEnter() {
+    if(this.alert.includes("copyVerif--show")) return;
+    this.alert = "copyVerif copyVerif--show";
+    this.alertText = "Cliquez pour copier";
+  }
+
+  onMouseLeave() {
+    if(this.alert.includes("copyVerif--copied copyVerif--show")) return
+    else this.alert = "copyVerif";
+  }
+
+  onCopy(text:string) {
+    if(this.alert.includes("copyVerif--copied copyVerif--show")) return;
+    this.clip.copy(text);
+    this.alert = "copyVerif copyVerif--copied copyVerif--show";
+    this.alertText = "Copié !"
+    clearTimeout(1);
+    setTimeout(() => {
+      this.alert = "copyVerif copyVerif--copied";
+    }, 1000);
+  }
 }
