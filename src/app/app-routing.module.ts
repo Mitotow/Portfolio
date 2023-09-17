445bb40a7b579b2c12a165d394918eaa49379a52
Thomas <thomas@Thomas-HP-Laptop.home>
2023-09-17 10:55:57 +0200

    Adding routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SkillsPageComponent } from './skills-page/skills-page.component';
+import { NotfoundPageComponent } from './notfound-page/notfound-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'skills',
+    component: SkillsPageComponent,
+  },
+  {
+    path: '**',
+    component: NotfoundPageComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
