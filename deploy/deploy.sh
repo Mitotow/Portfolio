3d12032b123a79ba205b997d76cca6c522f2565d
E224340Y <thomas.ambroise@etu.univ-nantes.fr>
2025-10-05 01:31:05 +0200

    chore: remove deploy folder

--- deploy/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git pull
-npm install
-npm run build
-podman build -f $(pwd)/Podmanfile -t portfolio-httpd
-podman run --replace -d -it -p 8080:80 --name "webserver-portfolio" portfolio-httpd
