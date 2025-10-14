84ea1ee92c008ecbe859d06d2fb1d334e8c911ba
E224340Y <thomas.ambroise@etu.univ-nantes.fr>
2025-10-14 23:41:56 +0200

    chore: change cache ttl to 1 hour

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 export const constants = {
-    API_URL: "https://api.thomas-ambroise.fr/api/v1",
+    API_URL: "http://127.0.0.1:8080/api/v1",
     API_PROJECTS_ENDPOINT: "/projects",
     API_SKILLS_ENDPOINT: "/skills",
     GITHUB_URL: "https://github.com/Mitotow",
     LINKEDIN_URL: "https://www.linkedin.com/in/thomas-ambroise-57971325b/",
-    CACHE_TTL: 1 * 60 * 1000, // 1 minute
+    CACHE_TTL: 60 * 60 * 1000, // 1 hour
 };
